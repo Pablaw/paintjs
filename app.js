c5dfae11e9041dedcefb34fdae2ca4e804697166
Pablaw <chulhwa7@gmail.com>
2021-11-27 18:16:35 +0900

    Nov 4th Sat Sturdy

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const btn = document.querySelector('.test');
+const body = document.querySelector('body');
+
+
+function solution (a) {
+    console.log(a);
+}
+
+if(body) {
+    body.addEventListener('mousemove', solution);
+}
